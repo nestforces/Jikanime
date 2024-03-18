4f05a4f0d89440b9e31a429d9b1a462bf2e31622
nestforces <maulanamahfud2113@gmail.com>
2024-03-18 15:50:33 +0700

    create-jikanime

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path = '/Jikanime' element={<Home />}/>
+          <Route path = '/' element={<Home />}/>
           <Route path = '/anime-detail/:id' element={<AnimeDetail />}/>
         </Routes>
     </>
